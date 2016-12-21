7e798d70daea050cc8599331a2af565381e50637
jordan4ibanez <jordan4ibanez@gmail.com>
2016-12-21 16:40:51 -0500

    Shift click to link

--- leash.lua
+++ leash.lua
@@ -8,18 +8,26 @@ minetest.register_craftitem("open_ai:leash", {
 		if pointed_thing.type == "object" then
 			local object = pointed_thing.ref
 			local name   = user:get_player_name()
-			if open_ai.leash_table[name] == nil then
-				open_ai.leash_table[name] = object
-				
+			--player sneaks to connect mobs together
+			if user:get_player_control().sneak == true then
+				if open_ai.leash_table[name] == nil then
+					minetest.chat_send_player(name, "Click other mob to link")
+					open_ai.leash_table[name] = object
+					--object:get_luaentity().target = user
+					--object:get_luaentity().leashed = true
+				else
+					minetest.chat_send_player(name, "Linked!")
+					open_ai.leash_table[name]:get_luaentity().target = object
+					open_ai.leash_table[name]:get_luaentity().leashed = true
+					open_ai.leash_table[name] = nil
+				end
+			else --just connect to player
+				if open_ai.leash_table[name] ~= nil then
+					minetest.chat_send_player(name, "Link failed!")
+					open_ai.leash_table[name] = nil
+				end
 				object:get_luaentity().target = user
 				object:get_luaentity().leashed = true
-				
-				print("got first")
-			else
-				print("got second")
-				open_ai.leash_table[name]:get_luaentity().target = object
-				open_ai.leash_table[name]:get_luaentity().leashed = true
-				open_ai.leash_table[name] = nil
 			end
 		end
 	end,
